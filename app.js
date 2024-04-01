f77e9cf4f66dd0d824944dd0905dcc3c853306f8
Slim-us <66728561+Slim-us@users.noreply.github.com>
2024-03-31 19:48:46 -1100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+require('dotenv/config');
+
+const authJwt = require('./helpers/jwt');
+const errorHandler = require('./helpers/error-handler');
+
+app.use(cors()); //! ahaya fazet il cors
+app.options('*',cors());
+
+// Middlewares
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
+app.use(morgan('tiny'));
+// app.use(authJwt());
+// app.use('/public/uploads', express.static( __dirname + '/public/uploads'));
+app.use(errorHandler);
+
+const api = process.env.API_URL;
+const categoriesRoute = require('./routes/categories');
+const userRoute = require('./routes/users');
+const mapRoute = require('./routes/mapController');
+
+// Routes
+// localhost:3000/api/categories fil postman 
+app.use(`${api}/categories`, categoriesRoute);
+app.use(`${api}/users`, userRoute);
+app.use(`${api}/map`, mapRoute);
+
+const dbConfig = require('./config/database.config.js');
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+
+}).then(() => {
+    console.log("Successfully connected to the database");
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
+// listen for requests
+app.listen(3000, () => {
+    console.log("Server is listening on port "+api+" 3000");
+});
---
+++ config/database.configexample.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url:'mongodb+srv://MrRamyy:M7yH4YBKglMHAGlM@cluster0.tst5fyr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+}
